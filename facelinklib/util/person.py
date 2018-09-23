fad34bdae4986dfee46790b446376d69f252b8aa
Advaith HL <niceadvaith@gmail.com>
2018-09-23 21:46:53 +0530

    Add Person

---
+++ facelinklib/util/person.py
@@ -0,0 +1,10 @@
+
+
+class Person:
+    """
+    Person is the implementation of a real-life person. Every Person has at
+    least one Portrait which is identified by the functions.
+    """
+
+    def __init__(self):
+        self.name = None
