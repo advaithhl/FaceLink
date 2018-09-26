422b0607e9415344db22c4970867d47e0df3bc92
Advaith HL <niceadvaith@gmail.com>
2018-09-26 12:58:54 +0530

    Changed Photo

--- facelinklib/util/photo.py
+++ facelinklib/util/photo.py
@@ -1,21 +1,31 @@
-from PIL.Image import Image
+from pathlib import Path
 
 
-class Photo(Image):
+class Photo:
     """
     Photo is the representation of a single photo given to be processed.
     Every photo given as input is instantiated into a Photo object, which is
     passed to other functions to extract faces.
     """
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, path: Path):
+        self.path = path
+
+    @property
+    def filename(self):
+        return self.path.name.split('.')[0]
+
+    @property
+    def extension(self):
+        return self.path.name.split('.')[-1]
 
     def tostring(self, *args, **kw):
-        pass
+        raise NotImplementedError("tostring() has been removed. "
+                                  "Please call tobytes() instead.")
 
     def fromstring(self, *args, **kw):
-        pass
+        raise NotImplementedError("fromstring() has been removed. "
+                                  "Please call frombytes() instead.")
 
     def offset(self, xoffset, yoffset=None):
         pass
