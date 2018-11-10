a7215561437b273b9e1d876c1ae530501e631c5e
Advaith HL <niceadvaith@gmail.com>
2018-11-10 19:15:41 +0530

    Person: Add property name

--- facelinklib/util/person.py
+++ facelinklib/util/person.py
@@ -6,5 +6,9 @@ class Person:
     least one Portrait which is identified by the functions.
     """
 
-    def __init__(self):
-        self.name = None
+    def __init__(self, name: str):
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
