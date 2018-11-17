788a4bf25a329cde9ea6326e7fc30eb699a2c683
Advaith HL <niceadvaith@gmail.com>
2018-11-17 10:52:13 +0530

    Get links between people

--- facelinklib/core/analysis.py
+++ facelinklib/core/analysis.py
@@ -1,5 +1,7 @@
 import networkx as nx
 
+from facelinklib.util.person import Person
+
 
 def shortest_link(g: nx.MultiGraph, person1, person2):
     try:
@@ -7,3 +9,8 @@ def shortest_link(g: nx.MultiGraph, person1, person2):
         return sp
     except nx.exception.NodeNotFound as mpe:
         print(mpe)
+
+
+def get_links_between(g, person1, person2):
+    links = g[person1][person2]
+    return (links[i]['photo'] for i in range(len(links)))
