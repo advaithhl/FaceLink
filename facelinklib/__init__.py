11453df0884fb079504be20e981662da3b7753cd
Advaith HL <niceadvaith@gmail.com>
2018-11-17 16:04:10 +0530

    Fix with Run mode 2

--- facelinklib/__init__.py
+++ facelinklib/__init__.py
@@ -16,4 +16,10 @@ if RUN_MODE == 1:
         new_name = input("Please enter another name for the results folder : ")
         RESULTS = HOME.joinpath(new_name)
 
+if RUN_MODE == 2 and not RESULTS.exists():
+        print('- You are trying to analyse photos without providing results from the first stage.')
+        print('- Please give a folder containing pictures, so that I can identify faces from them.')
+        print('- You can start analysis after I identify the faces.')
+        exit()
+
 print("+ Using {} as results folder".format(RESULTS))
