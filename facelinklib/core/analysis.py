8c7ca53ece74a7a28e9b1cec383391320b78ea15
Advaith HL <niceadvaith@gmail.com>
2018-11-17 13:23:11 +0530

    Add functions to list people

--- facelinklib/core/analysis.py
+++ facelinklib/core/analysis.py
@@ -14,3 +14,11 @@ def shortest_link(g: nx.MultiGraph, person1, person2):
 def get_links_between(g, person1, person2):
     links = g[person1][person2]
     return (links[i]['photo'] for i in range(len(links)))
+
+
+def get_all_related_people(g: nx.MultiGraph, person):
+    return g.neighbors(person)
+
+
+def get_all_people(g):
+    return (person for person in g.nodes)
