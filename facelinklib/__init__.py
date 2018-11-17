5fbe22c251d7e73dbb3df3784609926e79c0c775
Advaith HL <niceadvaith@gmail.com>
2018-11-17 13:14:54 +0530

    Results folder duplication fix

--- facelinklib/__init__.py
+++ facelinklib/__init__.py
@@ -1,4 +1,20 @@
 from pathlib import Path
+from sys import argv
 
 HOME = Path('.')
 RESULTS = Path('{}/results/'.format(HOME))
+
+
+def results_dir_safe():
+    cnt = 2
+    global RESULTS
+    while RESULTS.exists():
+        RESULTS = Path('{}/results{}/'.format(HOME, str(cnt)))
+        cnt += 1
+    if len(argv) < 2:
+        cnt -= 2
+        RESULTS = Path('{}/results{}/'.format(HOME, str(cnt)))
+    print("+ Using folder {} as results folder".format(RESULTS))
+
+
+results_dir_safe()
